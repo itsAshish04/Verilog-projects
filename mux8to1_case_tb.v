20ac3ae265e3578ecd03eba2dfca994f4f2205a1
itsAshish04 <a7052526854@gmail.com>
2026-07-11 16:51:27 +0530

    Add files via upload

---
+++ mux8to1_case_tb.v
@@ -0,0 +1,36 @@
+`timescale 1ns/1ps
+
+module mux8to1_case_tb;
+
+reg [7:0] I;
+reg [2:0] S;
+wire Y;
+
+mux8to1_case DUT(
+    .I(I),
+    .S(S),
+    .Y(Y)
+);
+
+initial
+begin
+    $dumpfile("mux8to1_case.vcd");
+    $dumpvars(0, mux8to1_case_tb);
+
+    I = 8'b10110010;
+
+    S = 3'b000; #10;
+    S = 3'b001; #10;
+    S = 3'b010; #10;
+    S = 3'b011; #10;
+    S = 3'b100; #10;
+    S = 3'b101; #10;
+    S = 3'b110; #10;
+    S = 3'b111; #10;
+
+    $finish;
+end
+
+endmodule
+
+   
